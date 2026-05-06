a6816a1e36269dc9b39fa678d7d8dea54a9e20a3
Jay Nabonne <jaynabonne@gmail.com>
2026-05-06 13:16:31 +0100

    Remove some code from Fuzzy_Not that no longer has a test and shouldn't be needed.

--- src/fuzzy.adb
+++ src/fuzzy.adb
@@ -2,7 +2,7 @@ package body Fuzzy is
 
    function Fuzzy_Not (F : Fuzzy_Value) return Fuzzy_Value is
    begin
-      return Fuzzy_Value'Max (Fuzzy_Value'Min (1.0 - F, 1.0), 0.0);
+      return Fuzzy_Value'Min (1.0 - F, 1.0);
    end Fuzzy_Not;
 
 end Fuzzy;
