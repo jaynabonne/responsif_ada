54c3e8901c4ba8e1b9ea7cc29f6b6eab1cd9d2e2
Jay Nabonne <jaynabonne@gmail.com>
2026-05-19 19:59:15 +0100

    Add numeric expression evaluation test

--- tests/expression-test_evaluate.adb
+++ tests/expression-test_evaluate.adb
@@ -30,12 +30,29 @@ package body Expression.Test_Evaluate is
       Assert_Equal (Result, 10.0, "should evaluate to 10.0");
    end Test_Evaluate_Variable_Expression;
 
+   procedure Test_Evaluate_Numeric_Expression
+     (T : in out AUnit.Test_Cases.Test_Case'Class with Unreferenced)
+   is
+      Steps : Expression.Steps.Compiled_Steps;
+      Result : Float;
+   begin
+      Steps.Append (Expression.Steps.Create_Numeric_Step (314.0));
+      Result := Expression.Evaluate.Evaluate_Expression (
+         Steps, Test_Lookup'Access
+      );
+      Assert_Equal (Result, 314.0, "should evaluate to 314.0");
+   end Test_Evaluate_Numeric_Expression;
+
    overriding procedure Register_Tests (T : in out Test_Case) is
       use AUnit.Test_Cases.Registration;
    begin
       Register_Routine (
          T, Test_Evaluate_Variable_Expression'Access,
          "Evaluates a compiled variable expression"
+      );
+      Register_Routine (
+         T, Test_Evaluate_Numeric_Expression'Access,
+         "Evaluates a compiled numeric expression"
       );
    end Register_Tests;
 
