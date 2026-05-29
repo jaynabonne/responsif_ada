4a6f05a27f992d445d9166cecd4190b964c1ae92
Jay Nabonne <jaynabonne@gmail.com>
2026-05-29 22:23:00 +0100

    Some compilation splitting tests

--- src/expression/expression-compilation.adb
+++ src/expression/expression-compilation.adb
@@ -1,10 +1,69 @@
+with Ada.Strings.Fixed; use Ada.Strings.Fixed;
+
 package body Expression.Compilation is
+
+   type Token_Type is (None, Space, Identifier, Special);
+
+   function Is_Identifier (C : Character) return Boolean is
+   begin
+      return
+         (C in 'A' .. 'Z') or else
+         (C in 'a' .. 'z') or else
+         (C in '0' .. '9') or else
+         (C in '_' | '.' | '$' | ':');
+   end Is_Identifier;
+
+   function Is_Space (C : Character) return Boolean is
+   begin
+      return C = ' ';
+   end Is_Space;
+
+   function Type_Of (C : Character) return Token_Type is
+   begin
+      if Is_Space (C) then
+         return Space;
+      elsif Is_Identifier (C) then
+         return Identifier;
+      else
+         return Special;
+      end if;
+   end Type_Of;
 
    function Components_Of (Source : String)
          return Component_Vectors.Vector
    is
       Result : Component_Vectors.Vector;
+      Index : Natural := Source'First;
+      Token : String (1 .. Source'Length);
+      Token_Index : Natural := Token'First - 1;
+      Current_Type : Token_Type := None;
+
+      procedure Flush_Part
+      is
+         S : constant String := Token (Token'First .. Token_Index);
+         Trimmed : constant String := Trim (S, Ada.Strings.Both);
+      begin
+         if Trimmed /= "" then
+            Result.Append (Trimmed);
+         end if;
+         Token_Index := Token'First - 1;
+         Current_Type := None;
+      end Flush_Part;
    begin
+      while Index <= Source'Last loop
+         if Current_Type = None then
+            Current_Type := Type_Of (Source (Index));
+         elsif Type_Of (Source (Index)) /= Current_Type then
+            Flush_Part;
+            Current_Type := Type_Of (Source (Index));
+         end if;
+         Token_Index := Token_Index + 1;
+         Token (Token_Index) := Source (Index);
+         Index := Index + 1;
+      end loop;
+      if Token_Index > 0 then
+         Flush_Part;
+      end if;
       return Result;
    end Components_Of;
 
--- tests/expression-test_compilation.adb
+++ tests/expression-test_compilation.adb
@@ -1,26 +1,91 @@
 with AUnit.Assertions; use AUnit.Assertions;
 with AUnit.Test_Cases; use AUnit.Test_Cases;
 with Test_Helpers; use Test_Helpers;
+with Test_Generics; use Test_Generics;
 
 with Expression.Compilation; use Expression.Compilation;
 
 package body Expression.Test_Compilation is
-   --  Test for splitting an empty expression
-   procedure Test_Uncompiled_Expression
+   procedure Assert_Equal is new Test_Generics.Generic_Assert_Equal
+     (T     => Component_Vectors.Vector,
+      Image => Component_Vectors.Vector'Image,
+      Equal =>  Component_Vectors."=");
+
+   procedure Test_Empty_Expression
      (T : in out AUnit.Test_Cases.Test_Case'Class with Unreferenced)
    is
-      Components : constant Compilation.Component_Vectors.Vector :=
-         Compilation.Components_Of ("");
+      Actual : constant Component_Vectors.Vector :=
+         Components_Of ("");
+      Expected : constant Component_Vectors.Vector :=
+         Component_Vectors.Empty_Vector;
    begin
-      Assert (Components.Is_Empty, "should be empty");
-   end Test_Uncompiled_Expression;
+      Assert_Equal (Actual, Expected, "should be empty");
+   end Test_Empty_Expression;
+
+   procedure Test_Identifier_Expression
+     (T : in out AUnit.Test_Cases.Test_Case'Class with Unreferenced)
+   is
+      Actual : constant Component_Vectors.Vector :=
+         Components_Of ("var1");
+      Expected : constant Component_Vectors.Vector := ["var1"];
+   begin
+      Assert_Equal (Actual, Expected, "should contain the identifier");
+   end Test_Identifier_Expression;
+
+   procedure Test_Identifier_Expression_With_Whitespace
+     (T : in out AUnit.Test_Cases.Test_Case'Class with Unreferenced)
+   is
+      Actual : constant Component_Vectors.Vector :=
+         Components_Of (" var1 ");
+      Expected : constant Component_Vectors.Vector := ["var1"];
+   begin
+      Assert_Equal (Actual, Expected, "should contain the identifier");
+   end Test_Identifier_Expression_With_Whitespace;
+
+   procedure Test_Expression_With_Unary_Operator
+     (T : in out AUnit.Test_Cases.Test_Case'Class with Unreferenced)
+   is
+      Actual : constant Component_Vectors.Vector :=
+         Components_Of ("not var1 ");
+      Expected : constant Component_Vectors.Vector := ["not", "var1"];
+   begin
+      Assert_Equal (Actual, Expected, "should contain the unary operator and identifier");
+   end Test_Expression_With_Unary_Operator;
+
+   procedure Test_Expression_With_Binary_Operator
+     (T : in out AUnit.Test_Cases.Test_Case'Class with Unreferenced)
+   is
+      Actual : constant Component_Vectors.Vector :=
+         Components_Of ("var1+var2 ");
+      Expected : constant Component_Vectors.Vector := ["var1", "+", "var2"];
+   begin
+      Assert_Equal (Actual, Expected, "should contain the binary operator and identifiers");
+   end Test_Expression_With_Binary_Operator;
+
+   --  ---------------------------------------------------------------------
 
    overriding procedure Register_Tests (T : in out Test_Case) is
       use AUnit.Test_Cases.Registration;
    begin
       Register_Routine (
-         T, Test_Uncompiled_Expression'Access,
+         T, Test_Empty_Expression'Access,
          "Test splitting an empty expression"
+      );
+      Register_Routine (
+         T, Test_Identifier_Expression'Access,
+         "Test splitting an identifier"
+      );
+      Register_Routine (
+         T, Test_Identifier_Expression_With_Whitespace'Access,
+         "Test splitting an identifier with whitespace"
+      );
+      Register_Routine (
+         T, Test_Expression_With_Unary_Operator'Access,
+         "Test splitting an expression with a unary operator"
+      );
+      Register_Routine (
+         T, Test_Expression_With_Binary_Operator'Access,
+         "Test splitting an expression with a binary operator"
       );
    end Register_Tests;
 
